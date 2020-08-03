2035a3908b5ebe9eee5469bb8043456d8023b4ad
sahilKamble <abg1203@gmail.com>
2020-08-03 21:35:52 +0530

    index.js now makes table

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,21 +1,64 @@
 
-var subject ="MP";
-var subjectId ="" ;
-var div = "";
-var attendees = ['sebin francis','paras','sahil2','kapse'];
-const url  = "https://attn-server.herokuapp.com/";
-var url_subj = url+"subjects?name="+subject;
+// var subject ="MP";
+// var subjectId ="" ;
+// var div = "";
+// var attendees = ['sebin francis','paras','sahil2','kapse'];
+// const url  = "https://attn-server.herokuapp.com/";
+// var url_subj = url+"subjects?name="+subject;
+// let request1 = new XMLHttpRequest();
+// request1.open("GET", url_subj);
+// request1.send();
+// request1.onload = () => {
+//     if(request1.status === 200){
+//         var jsonObj1 = JSON.parse(request1.responseText);
+//         console.log(jsonObj1);
+//         subjectId = jsonObj1[0]._id;
+//         console.log(subjectId);
+//         var url_subjectStudents = url+"subjects/"+subjectId+"/students";
+//         console.log(url_subjectStudents); 
+//         console.log('test');
+//     }
+
+// const url  = "https://attn-server.herokuapp.com/attn/table/5f257aecfe8b2700174a61a9";
+
+// async function req() {       
+//     let req = await fetch(url);
+//     let res = await req.json();
+//     var table = '';
+//     console.log(res.students.length);
+//     var rows = res.students.length;
+//     for(var r = -1; r < rows; r++) {
+//         table += '<tr>';
+//         for(var c = -2 ; c < res.dates.length; c++) {
+//             if(r === -1 && c === -1) table += '<td>' + " " + '</td>';
+//             else if(r === -1 ) {
+//                 var d = new Date(res.dates[c])
+//                 table += '<td>' + d.toLocaleString() + '</td>'
+//             }
+//             else if(c === -2) table += '<td>' + res.students[r].roll + '</td>';
+//             else if(c === -1) table += '<td>' + res.students[r].name + '</td>';
+//             else table += '<td>' + res.students[r].attn[c] + '</td>';
+//         }
+//         table += '</tr>';
+//     }
+//     document.write('<table border=1>' + table + '</table>');
+// }
+// req();
+
+
 let request1 = new XMLHttpRequest();
-request1.open("GET", url_subj);
+request1.open("GET", "https://attn-server.herokuapp.com/subjects");
 request1.send();
 request1.onload = () => {
-    if(request1.status === 200){
-        var jsonObj1 = JSON.parse(request1.responseText);
-        console.log(jsonObj1);
-        subjectId = jsonObj1[0]._id;
-        console.log(subjectId);
-        var url_subjectStudents = url+"subjects/"+subjectId+"/students";
-        console.log(url_subjectStudents); 
-        console.log('test');
+
+    if (request1.status === 200) {
+        console.log("loaded")
+        var jsonObj = JSON.parse(request1.responseText);
+        // console.log(jsonObj1);
+        var i = 2;
+        for (subject of jsonObj) {
+            console.log(subject)
+            
+        }
     }
 }
